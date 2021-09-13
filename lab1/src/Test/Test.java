30e0852248f5dea4e3aae9032defe5c2876b8cee
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-09-13 09:47:16 +0300

    init for lab1

---
+++ lab1/src/Test/Test.java
@@ -0,0 +1,19 @@
+package Test;
+
+/** Test.Test
+ * @author Tomas Giedraitis
+ * 5. Prekės Nuskaityti vartotojo pateiktą prekių kodų (String) sąrašą (po vieną kiekvienoje eilutėje).
+ * Prekių saugojimui naudoti klasę-konteinerį java.util.ArrayList. Kai vartotojas baigia įvedimą,
+ * peržiūrėti kodų sąrašą, išvedant "silpnuosius kodus" (t.y., tokius, kurie įeina kaip eilutė į kitą
+ * kodą tame sąraše)
+ */
+
+/** class comment */
+public class Test {
+    /** Main function
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
