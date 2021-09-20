e64401237e3b3a386d2705e28ec833cb3ca207ed
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-09-20 17:42:16 +0300

    init TextEditor

---
+++ TextEditor/src/texedit/main/Editor.java
@@ -0,0 +1,18 @@
+package texedit.main;
+
+/**
+ * @author Tomas Giedraitis
+ * <p>
+ * Text Editor
+ */
+
+public class Editor {
+    /**
+     * Main function
+     *
+     * @param args - ...
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
