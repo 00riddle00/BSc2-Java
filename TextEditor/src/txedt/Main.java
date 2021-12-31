62ce55d4f9077200987655be447abe624f7f178d
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-12-31 16:28:44 +0200

    Update javadoc for Main class, update gitignore

--- TextEditor/src/txedt/Main.java
+++ TextEditor/src/txedt/Main.java
@@ -4,14 +4,26 @@ import txedt.cmdeditor.CmdEditor;
 import txedt.guieditor.GUIEditor;
 
 /**
- * The entry point of the progrram
- * See also <a href="{@docRoot}/../Readme.md">Readme.md</a>
+ * The entry point of the program
  */
 public class Main {
 
     /**
      * From the main method either CMDEditor or GUIEditor can be run.
-     * @param args an array of command-line arguments for the application
+     * This method contains only one line of code which call any of the
+     * two text editors.
+     *
+     * Call either `CmdEditor.begin(args)` or `GUIEditor.begin(args)`
+     * from this method.
+     *
+     * For additional requirements to run GUIEditor on linux see
+     * <a href="{@docRoot}/../Readme.md">Readme.md</a>
+     *
+     * @param args an array of command-line arguments for the program.
+     *             There should be either no arguments or a single argument -
+     *             the name of the file to be opened with the editor.
+     *             If there are more arguments, the program will produce
+     *             an error.
      */
     public static void main(String[] args) throws Exception {
         //CmdEditor.begin(args);
