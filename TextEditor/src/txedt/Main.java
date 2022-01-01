53be58b942e4b9ee58a53795979b1247c15cd73c
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2022-01-01 23:03:40 +0200

    Update Main

--- TextEditor/src/txedt/Main.java
+++ TextEditor/src/txedt/Main.java
@@ -12,25 +12,41 @@ public class Main {
 
     /**
      * From the main method either CMDEditor or GUIEditor can be run.
-     * This method contains only one line of code which call any of the
-     * two text editors.
-     * <p>
-     * Call either `CMDEditor.begin(args)` or `GUIEditor.begin(args)`
-     * from this method.
      * <p>
      * For additional requirements to run GUIEditor on linux see
      * <a href="{@docRoot}/../Readme.md">Readme.md</a>
      *
-     * @param args an array of command-line arguments for the program.
-     *             There should be either no arguments or a single argument -
-     *             the name of the file to be opened with the editor.
+     * @param args command-line arguments for the program. If the optional
+     *             flag `--terminal` is passed as the first argument, the
+     *             CMDEditor will be run, else by default GUIEditor will
+     *             be launched.
+     *             <p>
+     *             Then as the second argument in case of CMDEditor, and the
+     *             first argument in case of GUIEditor, the file name can
+     *             be passed which to be opened with the editor.
+     *             <p>
      *             If there are more arguments, the program will produce
      *             an error.
      * @throws InterruptedException            can occur the Thread which opens or saves to a file is interrupted
      * @throws UnsupportedLookAndFeelException can occur when a chosen look and feel is not supported by the system
      */
     public static void main(String[] args) throws InterruptedException, UnsupportedLookAndFeelException {
-        //CMDEditor.begin(args);
-        GUIEditor.begin(args);
+
+        if (args.length == 0) {
+            GUIEditor.begin(args);
+        } else if (args.length > 2) {
+            System.out.println("[Error]: Too many arguments");
+            System.exit(1);
+        } else if (args[0].equals("--terminal")) {
+            if (args.length > 1) {
+                String[] _args = {args[1]};
+                CMDEditor.begin(_args);
+            } else {
+                String[] _args = {};
+                CMDEditor.begin(_args);
+            }
+        } else {
+            GUIEditor.begin(args);
+        }
     }
 }
